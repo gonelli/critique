ce93f937581a1a07e588db05a701f54472209664
Ameya Joshi <mcgenius2@gmail.com>
2019-07-04 12:16:00 -0500

    Update Review object framework

--- Critique/Review.swift
+++ Critique/Review.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class Review {
     
-    var poster: UIImageView!
-    var title: UILabel!
-    var score: UILabel!
-    var name: UILabel!
-    var review: UILabel!
+    var poster: UIImage? // not in database, pulled when necessary
+    var title: String? // stored as key to get info from OMDB
+    var score: Double // stored as float
+    var name: String // store user id, and get their public name
+    var review: String // stored as string
     
+    var movieID: String
+    var userID: String
+    var reviewID: String
     
-
+    init(reviewID: String) {
+        
+    }
+    
+    func movieIDToTitle(movieID: String) {
+        
+    }
+    
+    func getPoster(movieID: String) {
+        
+    }
+    
+    func getName(userID: String) {
+        
+    }
+    
+    func getReview(reviewID: String) {
+        
+    }
+    
 }
