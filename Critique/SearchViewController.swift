d438d5e69c35018a00ee908ba3f38ac097f6023f
Ameya Joshi <mcgenius2@gmail.com>
2019-07-04 14:07:20 -0500

    Added front end for searching movies

--- Critique/SearchViewController.swift
+++ Critique/SearchViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    let searchSegueIdentfier = "searchSegue"
+    
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Search"
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // use appropriate identifier string or variable
+        if segue.identifier == searchSegueIdentfier {
+            let resultVC = segue.destination as! MovieSearchResultViewController
+            
+            resultVC.searchResults = // list of strings from searching movieSearchBar.text
+        }
+    }
+    
+    // code to dismiss keyboard when user clicks on background
+    
+    func textFieldShouldReturn(textField:UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
     
 }
