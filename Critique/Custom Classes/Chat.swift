1277058170d04b61d30b8c2f4ff1f9c66c5fcc9d
acramer <acramerxc@utexas.edu>
2019-07-18 11:36:21 -0500

    minor changes

---
+++ Critique/Custom Classes/Chat.swift
@@ -0,0 +1,9 @@
+//
+//  Chat.swift
+//  Critique
+//
+//  Created by Andrew Cramer on 7/16/19.
+//  Copyright © 2019 Andrew Cramer Tony Gonelli James Jackson Ameya Joshi. All rights reserved.
+//
+
+import Foundation
