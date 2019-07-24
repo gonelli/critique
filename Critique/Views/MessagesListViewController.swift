110aea7951bb65c9dca0326d5d34a68f11bafe38
James Jackson <james.sorley.jackson@gmail.com>
2019-07-24 16:17:05 -0500

    merge fix

--- Critique/Views/MessagesListViewController.swift
+++ Critique/Views/MessagesListViewController.swift
@@ -12,67 +12,67 @@ import FirebaseCore
 import FirebaseFirestore
 
 class MessagesListViewController: UITableViewController {
-
-    var db: Firestore!
-    var directMessages: [Chat] = [] {
-        didSet {
-            self.tableView.reloadData() // Reload table after reviews are fetched
+  
+  var db: Firestore!
+  var directMessages: [Chat] = [] {
+    didSet {
+      self.tableView.reloadData() // Reload table after reviews are fetched
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Messages"
+    addRefreshView()
+    initializeFirestore()
+    getDirectMessages()
+  }
+  
+  @objc func refresh() {
+    getDirectMessages()
+  }
+  
+  func addRefreshView() {
+    let refresh = UIRefreshControl()
+    tableView.refreshControl = refresh
+    refresh.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
+  }
+  
+  func initializeFirestore() {
+    let settings = FirestoreSettings()
+    Firestore.firestore().settings = settings
+    db = Firestore.firestore()
+  }
+  
+  // Fetches messages
+  func getDirectMessages() {
+    var directMessages: [Chat] = []
+    db.collection("users").document("\(Auth.auth().currentUser!.uid)").getDocument { (document, error) in
+      if let myChats = document?.data()?["myChats"] as? [String] {
+        for chat in myChats {
+          directMessages.append(Chat(chat))
         }
+        self.directMessages = directMessages
+        self.tableView.refreshControl?.endRefreshing()
+      }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.title = "Messages"
-        addRefreshView()
-        initializeFirestore()
-        getDirectMessages()
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return directMessages.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath) as! ChatTableViewCell
+    cell.chat = directMessages[indexPath.row]
+    return cell
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    let nextVC = segue.destination as! ChatViewController
+    if segue.identifier == "toChat", let selectedRow = tableView.indexPathForSelectedRow {
+      nextVC.chat = directMessages[selectedRow.row]
     }
-    
-    @objc func refresh() {
-        getDirectMessages()
-    }
-    
-    func addRefreshView() {
-        let refresh = UIRefreshControl()
-        tableView.refreshControl = refresh
-        refresh.addTarget(self, action: #selector(self.refresh), for: .valueChanged)
-    }
-    
-    func initializeFirestore() {
-        let settings = FirestoreSettings()
-        Firestore.firestore().settings = settings
-        db = Firestore.firestore()
-    }
-    
-    // Fetches messages
-    func getDirectMessages() {
-        var directMessages: [Chat] = []
-        db.collection("users").document("\(Auth.auth().currentUser!.uid)").getDocument { (document, error) in
-            if let myChats = document?.data()?["myChats"] as? [String] {
-                for chat in myChats {
-                    directMessages.append(Chat(chat))
-                }
-                self.directMessages = directMessages
-                self.tableView.refreshControl?.endRefreshing()
-            }
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return directMessages.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "chatCell", for: indexPath) as! ChatTableViewCell
-        cell.chat = directMessages[indexPath.row]
-        return cell
-    }
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        let nextVC = segue.destination as! ChatViewController
-//        if segue.identifier == "toChat", let selectedRow = tableView.indexPathForSelectedRow {
-//            nextVC.chat = directMessages[selectedRow.row]
-//        }
-    }
-    
+  }
+  
 }
