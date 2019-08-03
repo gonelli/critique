8dbd8058ddbdd0672c464a5345ef58454289b32d
James Jackson <james.sorley.jackson@gmail.com>
2019-08-03 17:23:14 -0500

    expanded like and dislike

--- Critique/Views/ExpandedReviewTableViewController.swift
+++ Critique/Views/ExpandedReviewTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import NightNight
 
 class ExpandedReviewTableViewController: UITableViewController {
@@ -17,7 +18,10 @@ class ExpandedReviewTableViewController: UITableViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var posterImage: UIImageView!
     @IBOutlet weak var reviewCell: UITableViewCell!
-    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dislikeButton: UIButton!
+  @IBOutlet weak var likesLabel: UILabel!
+  
     var deligate: UIViewController?
     var expanedReview: Review? = nil
     var movieObject: Movie?
@@ -25,6 +29,8 @@ class ExpandedReviewTableViewController: UITableViewController {
     var tappedCriticName: String?
     let mixedNightBgColor = MixedColor(normal: 0xffffff, night: 0x222222)
     let mixedNightTextColor = MixedColor(normal: 0x000000, night: 0xdddddd)
+  
+    var db: Firestore!
     
     // Fill in review's elements - movie title, poster, critic name, etc.
     override func viewDidLoad() {
@@ -76,6 +82,31 @@ class ExpandedReviewTableViewController: UITableViewController {
         criticLabel.mixedTextColor = mixedNightTextColor
         scoreLabel.mixedTextColor = mixedNightTextColor
         movieLabel.mixedTextColor = mixedNightTextColor
+      
+        initializeFirestore()
+        let movieID = expanedReview!.imdbID ?? "0"
+        let criticID = expanedReview!.criticID ?? "0"
+        let ref = self.db.collection("reviews").document(criticID + "_" + movieID)
+        ref.getDocument { (document, error) in
+          if error == nil {
+            let liked = document!.data()!["liked"] as! [String]
+            let disliked = document!.data()!["disliked"] as! [String]
+            self.likesLabel.text = "\(liked.count - disliked.count)"
+            let userID = Auth.auth().currentUser?.uid
+            if liked.contains(userID ?? "") {
+              self.likeButton.setImage(UIImage(named: "liked"), for: .normal)
+              self.dislikeButton.setMixedImage(MixedImage(normal: UIImage(named: "dislike")!, night: UIImage(named: "dislike-white")!), forState: .normal)
+            } else if disliked.contains(userID ?? "") {
+              self.likeButton.setMixedImage(MixedImage(normal: UIImage(named: "like")!, night: UIImage(named: "like-white")!), forState: .normal)
+              self.dislikeButton.setImage(UIImage(named: "disliked"), for: .normal)
+            } else {
+              self.likeButton.setMixedImage(MixedImage(normal: UIImage(named: "like")!, night: UIImage(named: "like-white")!), forState: .normal)
+              self.dislikeButton.setMixedImage(MixedImage(normal: UIImage(named: "dislike")!, night: UIImage(named: "dislike-white")!), forState: .normal)
+            }
+          } else {
+            fatalError(error!.localizedDescription)
+          }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -103,7 +134,7 @@ class ExpandedReviewTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return max(reviewTextView.contentSize.height + 70, posterImage.frame.height + posterImage.frame.minY + 10)
+        return max(reviewTextView.contentSize.height + 125, posterImage.frame.height + posterImage.frame.minY + 10)
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -129,5 +160,85 @@ class ExpandedReviewTableViewController: UITableViewController {
             criticVC.accountName = self.tappedCriticName!
         }
     }
+  
+  @IBAction func like(_ sender: Any) {
+    let movieID = expanedReview!.imdbID ?? "0"
+    let criticID = expanedReview!.criticID ?? "0"
+    let ref = self.db.collection("reviews").document(criticID + "_" + movieID)
+    ref.getDocument { (document, error) in
+      if error == nil {
+        var liked = document!.data()!["liked"] as! [String]
+        var disliked = document!.data()!["disliked"] as! [String]
+        let userID = Auth.auth().currentUser!.uid
+        if liked.contains(userID) {
+          if let index = liked.firstIndex(of: userID) {
+            self.likeButton.setMixedImage(MixedImage(normal: UIImage(named: "like")!, night: UIImage(named: "like-white")!), forState: .normal)
+            self.dislikeButton.setMixedImage(MixedImage(normal: UIImage(named: "dislike")!, night: UIImage(named: "dislike-white")!), forState: .normal)
+            liked.remove(at: index)
+          }
+        }
+        else {
+          if disliked.contains(userID) {
+            if let index = disliked.firstIndex(of: userID) {
+              disliked.remove(at: index)
+            }
+          }
+          self.likeButton.setImage(UIImage(named: "liked"), for: .normal)
+          self.dislikeButton.setMixedImage(MixedImage(normal: UIImage(named: "dislike")!, night: UIImage(named: "dislike-white")!), forState: .normal)
+          liked.append(userID)
+        }
+        ref.setData(["liked": liked, "disliked": disliked], merge: true)
+        self.likesLabel.text = "\(liked.count - disliked.count)"
+      }
+      else {
+        fatalError(error!.localizedDescription)
+      }
+    }
+  }
+  
+  @IBAction func dislike(_ sender: Any) {
+    let movieID = expanedReview!.imdbID ?? "0"
+    let criticID = expanedReview!.criticID ?? "0"
+    let ref = self.db.collection("reviews").document(criticID + "_" + movieID)
+    ref.getDocument { (document, error) in
+      if error == nil {
+        // When follow pressed
+        var liked = document!.data()!["liked"] as! [String]
+        var disliked = document!.data()!["disliked"] as! [String]
+        let userID = Auth.auth().currentUser!.uid
+        
+        if disliked.contains(userID) {
+          if let index = disliked.firstIndex(of: userID) {
+            self.likeButton.setMixedImage(MixedImage(normal: UIImage(named: "like")!, night: UIImage(named: "like-white")!), forState: .normal)
+            self.dislikeButton.setMixedImage(MixedImage(normal: UIImage(named: "dislike")!, night: UIImage(named: "dislike-white")!), forState: .normal)
+            disliked.remove(at: index)
+          }
+        }
+        else {
+          if liked.contains(userID) {
+            if let index = liked.firstIndex(of: userID) {
+              liked.remove(at: index)
+            }
+          }
+          self.likeButton.setMixedImage(MixedImage(normal: UIImage(named: "like")!, night: UIImage(named: "like-white")!), forState: .normal)
+          self.dislikeButton.setImage(UIImage(named: "disliked"), for: .normal)
+          disliked.append(userID)
+        }
+        ref.setData(["liked": liked, "disliked": disliked], merge: true)
+        self.likesLabel.text = "\(liked.count - disliked.count)"
+      }
+      else {
+        fatalError(error!.localizedDescription)
+      }
+    }
+  }
+  
+  
+  func initializeFirestore() {
+    let settings = FirestoreSettings()
+    Firestore.firestore().settings = settings
+    db = Firestore.firestore()
+  }
+  
 }
 
